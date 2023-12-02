c50e8eb7c3ddca3d1644ccb0984551af3c3fccaa
dneis2489 <denis-pelenev@inbox.ru>
2023-12-03 01:53:44 +0500

    Добавил пагинацию в виде динамической подгрузки

--- src/components/poll_list.js
+++ src/components/poll_list.js
@@ -8,14 +8,42 @@ function GetPollList(){
 
   //polls - хранит данные из API,  setPolls - записывает данные из API. Так как API возвращает массив, то начальное значение - это пустой массив
   const [polls, setPolls] = useState([]);
+  //текущая страница
+  const [currentPage, setCurrentPage] = useState(0)
+  //Состояние на подгрузку данных
+  const[fetching, setFetching] = useState(true)
+
+ //Состояние на подгрузку данных
+ const[totalCount, setTotalCount] = useState(0)
+  
 
   useEffect(() => {
-    axios
-      .get('http://localhost:8080/poll/filtered_list?sort=name&limit=20&page=0')
+    if(fetching){
+      axios
+      .get(`http://localhost:8080/poll/filtered_list?sort=name&limit=15&page=${currentPage}`)
       .then(data => {
-        setPolls(data.data.items);
+        setPolls([...polls, ...data.data.items]); //Чтобы не перезатирались данные, мы разворачиваем массив в который записываем старые данные и новые
+      setCurrentPage(currentPage + 1)
+      setTotalCount(data.data.totalCount)
       })
-  }, [])
+      .finally(() => setFetching(false));
+    } 
+  }, [fetching])
+
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler)
+    return function (){
+      document.removeEventListener('scroll', scrollHandler)
+    }
+  })
+
+  const scrollHandler = (e) => {
+    if(e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100 && polls.length < totalCount)
+    {
+      setFetching(true);
+    }
+  }
 
   return (
     <div className='polls'>
