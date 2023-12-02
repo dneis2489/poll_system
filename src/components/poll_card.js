84a9c34d2fd86cc6611ea9be1c887bf347aacc4a
dneis2489 <denis-pelenev@inbox.ru>
2023-12-02 18:13:26 +0500

    Подгр. дан. из api. Ред. даты под час. пояс польз.

--- src/components/poll_card.js
+++ src/components/poll_card.js
@@ -2,9 +2,51 @@ import React, { useState } from 'react';
 import '../css/poll_card.css';
 import Chat from './chat';
 
+
+//Преобразует дату полученную с бэка под часовой пояс клиента
+const convertDate = (dateString) => {
+  const date = new Date(dateString);
+
+  // Определяем часовой пояс клиента
+  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  const offsetInMinutes = date.getTimezoneOffset();
+
+  // Вычисляем смещение часового пояса клиента в миллисекундах
+  const offsetInMilliseconds = offsetInMinutes * 60 * 1000;
+
+  // Прибавляем смещение часового пояса к дате
+  const convertedDate = new Date(date.getTime() - offsetInMilliseconds);
+
+  // Получаем компоненты даты и времени
+  const day = String(convertedDate.getDate()).padStart(2, '0');
+  const month = String(convertedDate.getMonth() + 1).padStart(2, '0');
+  const year = convertedDate.getFullYear();
+  const hours = String(convertedDate.getHours()).padStart(2, '0');
+  const minutes = String(convertedDate.getMinutes()).padStart(2, '0');
+
+  // Формируем строку с датой, временем и часовым поясом
+  const formattedDate = `${day}.${month}.${year} ${hours}:${minutes}`;
+
+  return formattedDate;
+};
+
+const convertStatus = (status) => {
+  if(status == 'active')
+  {
+    return "Активен"
+  } 
+  else if (status == 'planned')
+  {
+    return 'Запланирован'
+  }
+  else
+  {
+    return 'Завершен'
+  }
+}
+
+
 const PollCard = ({ pollData }) => {
-
-
   const [expanded, setExpanded] = useState(false);
 
   const handleExpand = () => {
@@ -27,21 +69,21 @@ const PollCard = ({ pollData }) => {
     
   return (
     <div className={`poll_card ${expanded ? 'expanded' : ''}`}>
-      <div className="poll_header_card">
+      <div className="poll_header_card" onClick={handleExpand}>
         
         <div className="left_headder_card">
           <div className="name text_header_card">{pollData.name}</div>
           <div className="date text_header_card">
-            Сроки проведения: {pollData.start_date} - {pollData.end_date}
+            Сроки проведения: {convertDate(pollData.startDate)} - {convertDate(pollData.endDate)}
           </div>
         </div>
         
         <div className="right_headder_card">
-          <div className="status text_header_card">Статус: {pollData.status}</div>
+          <div className="status text_header_card">Статус: {convertStatus(pollData.status)}</div>
           <div className="users_in_poll text_header_card">
-            Проголосовало: {pollData.voited} /{pollData.people}
+            Проголосовало: {pollData.numberVotes} /{pollData.maxNumberVoted}
           </div>
-          <div className="expand_button" onClick={handleExpand}>
+          <div className="expand_button" >
             {expanded ? 'Свернуть' : 'Развернуть'}
           </div>
         </div>
--- src/components/poll_list.js
+++ src/components/poll_list.js
@@ -1,56 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PollCard from './poll_card';
 import '../css/poll_card.css'
+import axios from 'axios';
 
-//Тестовые данные
-const list = [
-    {
-        name: 'Сбор средств на уборку придомовой територии от снега', status: 'Активен', start_date: '22.11.2023',
-        end_date: '01.12.2023', people: 100, voited: 22, isVoited: true, description: 'Необходимо собрать деньги на уборку придомовой территории от снега',
-        creator: 'admin'
-    },
-    {
-        name: 'Сбор средств на постройку площадки', status: 'Активен', start_date: '22.11.2023',
-        end_date: '01.12.2023', people: 100, voited: 22, isVoited: true, description: 'Необходимо собрать деньги на постройку детской площадки',
-        creator: 'admin'
-    },
-    {
-        name: 'Сбор средств на уборку мусора', status: 'Активен', start_date: '22.11.2023',
-        end_date: '01.12.2023', people: 100, voited: 22, isVoited: true, description: 'Необходимо собрать деньги на уборку придомовой территории от мусора',
-        creator: 'admin'
-    }
-]
 
-//Сборка списка с опросами из объекта
-const Polls = () => {
-    const handleVote = (poll) => {
-      // Обработчик для голосования
-      alert(`Голос за опрос ${poll.name}`);
-    };
-  
-    const handleChatClick = () => {
-      // Обработчик для открытия чата
-      alert('Открываем чат');
-    };
-  
-    return (
-      <div>
-        {list.map((item, index) => (
-          <div key={index} className="poll_element">
-            {/* данные опроса и обработчики событий в компонент PollCard */}
-            <PollCard pollData={item} handleVote={handleVote} handleChatClick={handleChatClick} />
-          </div>
-        ))}
-      </div>
-    );
-  };
+function GetPollList(){
+
+  //polls - хранит данные из API,  setPolls - записывает данные из API. Так как API возвращает массив, то начальное значение - это пустой массив
+  const [polls, setPolls] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/poll/filtered_list?sort=name&limit=5&page=0')
+      .then(data => {
+        setPolls(data.data.items);
+      })
+  }, [])
+
+  return (
+    <div className='polls'>
+      {polls.map(poll => (
+        <div key={poll.id} className="poll_element">
+          {/* данные опроса и обработчики событий в компонент PollCard */}
+          <PollCard pollData={poll} />
+        </div>
+      ))}
+    </div>
+  );
+
+}
+
 
 //Список с опросами
 const pollList = () => {
     return(
-        <div className='polls'>
-            <Polls/>
-        </div>
+      GetPollList()
     )
 }
 
